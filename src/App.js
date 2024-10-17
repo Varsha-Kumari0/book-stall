be06d5882e41544a71145093ef6ff2011aeec1e8
Shubham <shubhamsinghmau2@gmail.com>
2024-10-17 17:49:33 +0530

    more books

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import { books } from './data';
 
 const BookCard = ({ book }) => (
   <div className="book-card">
-    <img src={book.img} alt={book.name} title='test'/>
-    <p>Stall price: {book.stall_price}</p>
-    <p>Pre-booking price: {book.pre_booking_price}</p>
+    <img src={book.img} alt={book.name} title={book.name} />
+    <h3 className="book-title">{book.name}</h3>
+    <p>Stall price: {book.stall_price} INR</p>
+    <p>Pre-booking price: {book.pre_booking_price} INR</p>
     <button>Pre Booking</button>
   </div>
 );
@@ -15,7 +16,7 @@ const BookStall = () => (
   <div className="book-stall">
     <header>
       <h1>Book Stall</h1>
-      <h2>Pre Booking offer : 10% Discount</h2>
+      <h2>Pre Booking offer: 10% Discount</h2>
     </header>
     <div className="book-list">
       {books.map((book, index) => (
