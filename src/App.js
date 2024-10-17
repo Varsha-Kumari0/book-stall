7f76c82019c7e6d1e8a4a728a0fc6c7fb23b1569
Shubham <shubhamsinghmau2@gmail.com>
2024-10-17 22:04:37 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const BookCard = ({ book }) => (
     <h3 className="book-title">{book.name}</h3>
     <p>Stall price: {book.stall_price} INR</p>
     <p>Pre-booking price: {book.pre_booking_price} INR</p>
+
     {book.isSold ? (
       <button className="sold">Sold</button>
     ) : (
@@ -29,6 +30,15 @@ const BookStall = () => (
     <header>
       <h1>Book Stall</h1>
       <h2>Pre Booking offer: 10% Discount</h2>
+      <div className="list-status">
+        <a
+          target="_blank"
+          className="list-link"
+          href="https://docs.google.com/spreadsheets/d/1A2br7qQwLFrkfgoAQ5GXO9U-BMs8NRJzQ-ZTmhjtrmM/edit?usp=sharing"
+        >
+          Book List Status
+        </a>
+      </div>
     </header>
     <div className="book-list">
       {books.map((book, index) => (
